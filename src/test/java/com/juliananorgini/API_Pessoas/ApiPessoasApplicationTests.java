3c7bf5d271908b97f4a4fb6d88a9948c9dc56d83
jnorgini <jnorgini@gmail.com>
2023-01-26 01:37:19 -0300

    Project created

---
+++ src/test/java/com/juliananorgini/API_Pessoas/ApiPessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juliananorgini.API_Pessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
