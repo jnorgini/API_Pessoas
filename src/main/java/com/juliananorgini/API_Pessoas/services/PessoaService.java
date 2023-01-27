752b6bbc93ba235fe3ae4970fabc55a0cb1be336
jnorgini <jnorgini@gmail.com>
2023-01-27 01:30:12 -0300

    Exception handling delete

--- src/main/java/com/juliananorgini/API_Pessoas/services/PessoaService.java
+++ src/main/java/com/juliananorgini/API_Pessoas/services/PessoaService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.juliananorgini.API_Pessoas.entities.Pessoa;
 import com.juliananorgini.API_Pessoas.repositories.PessoaRepository;
+import com.juliananorgini.API_Pessoas.services.exceptions.DatabaseException;
 import com.juliananorgini.API_Pessoas.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,13 @@ public class PessoaService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public Pessoa update(Long id, Pessoa obj) {
